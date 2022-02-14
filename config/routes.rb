fbea2de97c837dfde3de76319739d5c0028e3ab5
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-02-14 14:45:18 +0100

    modified stupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/ask', to: 'questions#ask' # controller name and  #name of the action localhost:3000/ask
+  get '/answer', to: 'questions#answer'
 end
